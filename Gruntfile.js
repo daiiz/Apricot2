67f58c1e8045c90df3c4668b6a350bd55b53226f
daiiz <da.iz.mg@gmail.com>
2015-09-07 14:24:38 +0900

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.initConfig({
+        browserify : {
+            dist : {
+                src : 'src/main.js',
+                dest : 'bundle.js'
+            }
+        },
+        watch : {
+            scripts : {
+                files : ['src/**/*.js'],
+                tasks : ['browserify']
+            }
+        }
+    });
+
+    Object.keys(pkg.devDependencies).forEach(function (devDependency) {
+        if (devDependency.match(/^grunt\-/)) {
+            grunt.loadNpmTasks(devDependency);
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+};
