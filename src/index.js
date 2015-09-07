6c3bbe72f3a363f275370d26e4e1ee890ece140d
daiiz <da.iz.mg@gmail.com>
2015-09-07 20:27:29 +0900

    index.jsを編集

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 var _ = require('underscore');
 
 // Apricot.*
-window.Apricot = _.extend(window.Apricot, {
+window.Apricot = _.extend((window.Apricot || {}), {
     Base : require('./Apricot/Base')
 });
