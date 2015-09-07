5fd5c06c950048bd8165aafb91adcd5ea163def0
daiiz <da.iz.mg@gmail.com>
2015-09-07 21:27:57 +0900

    名前空間Apricot.DocumentとApricot.Appを定義

--- www/bundle.js
+++ www/bundle.js
@@ -1575,4 +1575,16 @@ window.Apricot = _.extend((window.Apricot || {}), {
     Base : require('./Apricot/Base')
 });
 
+// Apricot.Document.*
+// Apricot UI を構築するためのAPIを展開する
+window.Apricot.Document = _.extend((window.Apricot.Document || {}), {
+
+});
+
+// Apricot.App.*
+// 構築された Apricot UI を操作するためのAPIを展開する
+window.Apricot.App = _.extend((window.Apricot.App || {}), {
+    
+});
+
 },{"./Apricot/Base":2,"underscore":1}]},{},[3]);
