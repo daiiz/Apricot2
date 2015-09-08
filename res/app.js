dbeb0504f3c45eed4f1349bf50718fbcce39a71f
daiiz <da.iz.mg@gmail.com>
2015-09-08 09:47:04 +0900

    変数名変更

--- res/app.js
+++ res/app.js
@@ -5,14 +5,14 @@ var Base = new Apricot.Base();
 
 // 第2引数に与えた図面ファイルから Apricot Zumen Model （AZM）を得る
 // 図面ファイルが省略された場合は空のAZMを返す
-var Azm = new Apricot.Document.Zumen({id: 'zumen1'});
+var z = new Apricot.Document.Zumen({id: 'zumen1'});
 
 // Apricot Module Model （AMM） を生成する
 // これで生成されるものはAZM.modulesの要素と同じデータ構造をもつ
-// var Amm = new Apricot.Document.Module({id: 'module1'});
+// var m = new Apricot.Document.Module({id: 'module1'});
 
 // 生成したAMMはAZMに追加しないと表示されない
-// Azm.addModule([Amm]);
+// z.addModule([Amm]);
 
 // Document Object Model （DOM）を取得する
-// var Dom = new Apricot.Document.Dom([Azm]);
+// var d = new Apricot.Document.Dom([Azm]);
