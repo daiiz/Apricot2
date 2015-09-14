2ace79455e42aa607877d9fbac7a118c91df3638
daiiz <da.iz.mg@gmail.com>
2015-09-15 00:22:04 +0900

    Chromeアプリ形式でとりあえず動くことを確かめた

--- www/background.js
+++ www/background.js
@@ -1,6 +1,6 @@
 // background.js for chrome apps
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+  chrome.app.window.create('foo.html', {
      width: 478, maxWidth: 478, height: 339, maxHeight: 339,
      singleton: false
   },function(appWindow) {
