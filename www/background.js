1145156b142997affe5a18b5cc811691954b6c81
daiiz <da.iz.mg@gmail.com>
2015-09-15 00:02:15 +0900

    ChromeApp化するために必要なものを決め打ちで用意

---
+++ www/background.js
@@ -0,0 +1,8 @@
+// background.js for chrome apps
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+     width: 478, maxWidth: 478, height: 339, maxHeight: 339,
+     singleton: false
+  },function(appWindow) {
+  });
+});
