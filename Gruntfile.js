cb8cd717535d917c40af7d50f06807c20f1f367f
daiiz <da.iz.mg@gmail.com>
2015-09-17 01:26:22 +0900

    不要なコメントを除去

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,6 @@ module.exports = function (grunt) {
     var pkg = grunt.file.readJSON('package.json');
 
     grunt.initConfig({
-        /*
-        browserify : {
-            dist : {
-                src : 'src/index.js',
-                dest : 'www/bundle.js'
-            }
-        },
-        */
         mochaTest: {
             test: {
                 src: ['test/**/*.js']
