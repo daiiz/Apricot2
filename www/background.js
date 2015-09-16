45f583380582ac761512b83a131f396f3f6387e0
daiiz <da.iz.mg@gmail.com>
2015-09-16 09:50:40 +0900

    www/を整理

--- www/background.js
+++ www/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-   chrome.app.window.create("bar.html", {
+   chrome.app.window.create("index.html", {
        width : 480, maxWidth : 480,
        height: 640, maxHeight: 640,
        singleton: false
