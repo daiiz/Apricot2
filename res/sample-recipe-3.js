bef345a3c500d708c0e7138cf54e5159c3b3e303
daiiz <da.iz.mg@gmail.com>
2015-09-18 00:04:34 +0900

    sample-recipe-3が古かったので直した

--- res/sample-recipe-3.js
+++ res/sample-recipe-3.js
@@ -1,9 +1,3 @@
-'use strict';
-
-var View = require('../src/Apricot/Document/View');
-var Zumen = require('../src/Apricot/Document/Zumen');
-var Brick = require('../src/Apricot/Document/Brick');
-
 var zumen = new Zumen({id: 'zumen'}, 'simple');
 zumen.addRecipe('design', {
     Visible: true
